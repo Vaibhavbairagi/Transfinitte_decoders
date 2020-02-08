5d61adc5380aac7d02279fddacb3aecf319e9945
Vaibhav Bairagi <vaibhavbairagi105@gmail.com>
2020-02-08 07:52:45 +0530

    Mainactivity sos wiring and slider with data wiring.

--- app/src/main/java/com/example/transfinitte_decoders/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/transfinitte_decoders/ui/home/HomeFragment.java
@@ -1,35 +1,187 @@
 package com.example.transfinitte_decoders.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.ViewPager;
 
+import com.example.transfinitte_decoders.Adapters.HomeSliderAdapter;
 import com.example.transfinitte_decoders.R;
+import com.example.transfinitte_decoders.pojos.HomeSliderPojo;
+import com.example.transfinitte_decoders.utils.MyBounceInterpolator;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private int CARD_COUNT, currentPage = 0;
+    private Context context;
+    private ViewPager viewPager;
+    private CardView cardView;
+    private Button sosButton;
+    private RelativeLayout bottom_sheet,bottom_sheet_bg,b1,b2;
+    private BottomSheetBehavior sheetBehavior;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
+        context = container.getContext();
+        viewPager = root.findViewById(R.id.home_view_pager);
+        cardView= root.findViewById(R.id.sos_card);
+        sosButton= root.findViewById(R.id.sos_button);
+        bottom_sheet= root.findViewById(R.id.home_bottom_sheet);
+        bottom_sheet_bg=root.findViewById(R.id.bottom_sheet_bg);
+        b1= root.findViewById(R.id.b1);
+        b2=root.findViewById(R.id.b2);
+        sheetBehavior=BottomSheetBehavior.from(bottom_sheet);
+
+        setUpViewPager();
+        sosButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                if(b2.getVisibility()!=View.VISIBLE){
+                    b2.setVisibility(View.VISIBLE);
+                }
+                if(b1.getVisibility()!=View.GONE){
+                    b1.setVisibility(View.GONE);
+                }
+                final Animation myAnim = AnimationUtils.loadAnimation(context, R.anim.bounce);
+                MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 20);
+                myAnim.setInterpolator(interpolator);
+                cardView.startAnimation(myAnim);
+                cardView.postOnAnimationDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (sheetBehavior.getState()!=BottomSheetBehavior.STATE_EXPANDED){
+                            //link onclick functions here
+                            sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                        }
+                    }
+                },500);
+
+            }
+        });
+        sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View view, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED:{
+
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_COLLAPSED: {
+                        bottom_sheet_bg.setVisibility(View.GONE);
+                    }
+                    break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View view, float v) {
+                bottom_sheet_bg.setVisibility(View.VISIBLE);
+                bottom_sheet_bg.setAlpha(v);
+            }
+        });
+        bottom_sheet_bg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
             }
         });
         return root;
+    }
+    private void setUpViewPager(){
+
+        ArrayList<HomeSliderPojo> cardComponents = new ArrayList<>();
+        fetchDiseaseDetails(cardComponents);
+        CARD_COUNT = cardComponents.size();
+        viewPager.setAdapter(new HomeSliderAdapter(context, cardComponents,bottom_sheet,bottom_sheet_bg,b1,b2));
+        final Handler handler = new Handler();
+        Timer swipeTimer = new Timer();
+        swipeTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (currentPage == CARD_COUNT) {
+                            currentPage = 0;
+                        }
+                        viewPager.setCurrentItem(currentPage++, true);
+                    }
+                });
+            }
+        }, 3000, 3000);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                currentPage = position;
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    private void fetchDiseaseDetails(ArrayList<HomeSliderPojo> cardComponents) {
+        cardComponents.add(new HomeSliderPojo(R.drawable.home_cold_disease_image,"COLD"));
+        ArrayList<String> a1= new ArrayList<>();
+        a1.add("1Remedy1");
+        a1.add("1Remedy2");
+        a1.add("1Remedy3");
+        a1.add("1Remedy4");
+        a1.add("1Remedy5");
+        cardComponents.get(0).setRemedies(a1);
+        cardComponents.add(new HomeSliderPojo(R.drawable.home_cough_disease_image,"COUGH"));
+        ArrayList<String> a2= new ArrayList<>();
+        a2.add("2Remedy1");
+        a2.add("2Remedy2");
+        a2.add("2Remedy3");
+        a2.add("2Remedy4");
+        a2.add("2Remedy5");
+        cardComponents.get(1).setRemedies(a2);
+        cardComponents.add(new HomeSliderPojo(R.drawable.home_fever_disease_image,"FEVER"));
+        ArrayList<String> a3= new ArrayList<>();
+        a3.add("3Remedy1");
+        a3.add("3Remedy2");
+        a3.add("3Remedy3");
+        a3.add("3Remedy4");
+        a3.add("3Remedy5");
+        cardComponents.get(2).setRemedies(a3);
+        cardComponents.add(new HomeSliderPojo(R.drawable.home_bodypain_disease_image,"BODY PAIN"));
+        ArrayList<String> a4= new ArrayList<>();
+        a4.add("4Remedy1");
+        a4.add("4Remedy2");
+        a4.add("4Remedy3");
+        a4.add("4Remedy4");
+        a4.add("4Remedy5");
+        cardComponents.get(3).setRemedies(a4);
     }
 }
