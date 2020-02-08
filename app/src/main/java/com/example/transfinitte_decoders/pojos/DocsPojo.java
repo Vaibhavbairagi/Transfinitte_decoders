59d16569a31330ebe0908240289003a61c9b422e
Vaibhav Bairagi <vaibhavbairagi105@gmail.com>
2020-02-08 19:44:26 +0530

    Doctors List Added with UI

--- app/src/main/java/com/example/transfinitte_decoders/pojos/DocsPojo.java
+++ app/src/main/java/com/example/transfinitte_decoders/pojos/DocsPojo.java
@@ -1,12 +1,13 @@
 package com.example.transfinitte_decoders.pojos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DocsPojo {
     String Department;
-    List<Docs> docs;
+    ArrayList<Docs> docs;
 
-    public DocsPojo(String department, List<Docs> docs) {
+    public DocsPojo(String department, ArrayList<Docs> docs) {
         Department = department;
         this.docs = docs;
     }
@@ -23,10 +24,11 @@ public class DocsPojo {
         return docs;
     }
 
-    public void setDocs(List<Docs> docs) {
+    public void setDocs(ArrayList<Docs> docs) {
         this.docs = docs;
     }
 
     public DocsPojo() {
+        docs = new ArrayList<>();
     }
 }
