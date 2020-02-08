0c35c40219556a38c7e41d9f03a571000b129f14
gouthamcm <goutham.898@gmail.com>
2020-02-08 12:24:47 +0530

    resolved errors

--- app/src/main/java/com/example/transfinitte_decoders/ui/dispenser/DispenserFragment.java
+++ app/src/main/java/com/example/transfinitte_decoders/ui/dispenser/DispenserFragment.java
@@ -1,28 +1,23 @@
-package com.example.transfinitte_decoders.ui.tools;
+package com.example.transfinitte_decoders.ui.dispenser;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.transfinitte_decoders.CustomAdapter;
 import com.example.transfinitte_decoders.DispenserAdapter;
 import com.example.transfinitte_decoders.MainActivity;
 import com.example.transfinitte_decoders.R;
 
-public class ToolsFragment extends Fragment {
+public class DispenserFragment extends Fragment {
 
-    private ToolsViewModel toolsViewModel;
+    private DispenserViewModel toolsViewModel;
     RecyclerView recyclerView;
     private static RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -30,6 +25,7 @@ public class ToolsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dispenser_fragment, container, false);
+
         recyclerView=(RecyclerView)view.findViewById(R.id.medicines_recycle);
         adapter = new DispenserAdapter(MainActivity.data_dispenser.getData());
         recyclerView.setAdapter(adapter);
